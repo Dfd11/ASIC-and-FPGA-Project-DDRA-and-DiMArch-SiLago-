90c06b277a470fbf976f920b5d2c7e71550833ba
SantiagoJacome-kth <jacome@kth.se>
2025-11-17 08:48:59 +0000

    Updated flat file

--- SiLagoNN/syn/scr/dc_flat.tcl
+++ SiLagoNN/syn/scr/dc_flat.tcl
@@ -26,55 +26,43 @@
 # The standard way of executing the is from the project_top_folder
 # with the following command
 #
-# $ dc_shell -f ../syn/scr/dc_flat.tcl
+# $ dc_shell -f ../syn/dc_flat.tcl
 ################################################################################
 
-source ../syn/synopsys_dc.setup
-
 #Directory variables
-set SOURCE_DIR ../rtl;                          # source directory with the rtl 
-set SYN_DIR ../syn;                             # synthesis directory
-set OUT_DIR ../db;                          # output directory for output files: netlist, sdf sdc.
-set REPORT_DIR ../syn/rpt;                      # report directory for synthesis reports on timing and area
+set REPORT_DIR ../rpt; 			# report directory for synthesis reports on timing and area
+set OUT_DIR ../db; 				# output directory for output files: netlist, sdf sdc.
+set SOURCE_DIR ../../rtl; 				# source directory with the rtl 
+set SYN_DIR ../; 				# synthesis directory
 
-source ${SYN_DIR}/synopsys_dc.setup
+source ${SYN_DIR}/scr/synopsys_dc.setup
 
 set TOP_NAME drra_wrapper
 
 # Read files
-# First the DesignWare Components
-set hierarchy_files [split [read [open ${SOURCE_DIR}/dware_hierarchy_verilog.txt r]] "\n"]
-foreach filename [lrange ${hierarchy_files} 0 end-1] {
-        puts "${filename}"
-        analyze -format verilog -lib WORK "${SOURCE_DIR}/${filename}"
-}
-
-#Then the design components
 set hierarchy_files [split [read [open ${SOURCE_DIR}/${TOP_NAME}_hierarchy.txt r]] "\n"]
 foreach filename [lrange ${hierarchy_files} 0 end-1] {
-        puts "${filename}"
-        analyze -format VHDL -lib WORK "${SOURCE_DIR}/${filename}"
+	puts "${filename}"
+	analyze -format VHDL -lib WORK "${SOURCE_DIR}/${filename}"
 }
 
 elaborate drra_wrapper
-
 link
 
-#set_wire_load_mode segmented
-#set_wire_load_model -name TSMC8K_Lowk_Aggresive
-#set_operating_condition NCCOM
+set_wire_load_mode segmented
+# set_wire_load_model -name TSMC8K_Lowk_Aggresive
+# set_operating_condition NCCOM
 
 source ${SYN_DIR}/constraints.sdc;
 
 compile -map_effort medium
 
-report_constraints      > ${REPORT_DIR}/${TOP_NAME}_constratints.sdc
-report_area             > ${REPORT_DIR}/${TOP_NAME}_area.txt
-report_cell             > ${REPORT_DIR}/${TOP_NAME}_cells.txt
-report_timing           > ${REPORT_DIR}/${TOP_NAME}_timing.txt
-report_power            > ${REPORT_DIR}/${TOP_NAME}_power.txt
+report_constraints 	> ${REPORT_DIR}/${TOP_NAME}_constratints.sdc
+report_area 		> ${REPORT_DIR}/${TOP_NAME}_area.txt
+report_cell 		> ${REPORT_DIR}/${TOP_NAME}_cells.txt
+report_timing 		> ${REPORT_DIR}/${TOP_NAME}_timing.txt
+report_power 		> ${REPORT_DIR}/${TOP_NAME}_power.txt
 
 # Export netlist
 write -hierarchy -format ddc -output ${OUT_DIR}/${TOP_NAME}.ddc
 write -hierarchy -format verilog -output ${OUT_DIR}/${TOP_NAME}.v
-
